6abdc480bf6db715e2b9195e083a7574b6d4ea45
Tlaster <tlaster@outlook.com>
2022-12-08 12:42:37 +0800

    fix script

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,7 +51,7 @@ label_when_approved() {
     fi
     
     if [[ "$APPROVALS" == "all" ]]; then
-      $APPROVALS = {#reviews[@]}
+      APPROVALS = {#reviews[@]}
     fi
 
     echo "${approvals}/${APPROVALS} approvals"
